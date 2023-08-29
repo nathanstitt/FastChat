1bca6d3b3ea1f468d9fcdc9623f5e2b07bd1a4dd
Lianmin Zheng <lianminzheng@gmail.com>
2023-08-29 18:36:30 +0000

    adjust sampling rate

--- fastchat/serve/gradio_block_arena_anony.py
+++ fastchat/serve/gradio_block_arena_anony.py
@@ -177,14 +177,13 @@ SAMPLING_WEIGHTS = {
     "wizardlm-13b": 1.5,
     # tier 2
     "codellama-13b-instruct": 1.0,
-    "guanaco-33b": 1.0,
     "vicuna-7b": 1.0,
     "llama-2-7b-chat": 1.0,
-    # tier 3
+    "chatglm2-6b": 1.0,
+    # deprecated
+    "guanaco-33b": 1.0,
     "fastchat-t5-3b": 0.5,
     "alpaca-13b": 0.5,
-    "chatglm2-6b": 0.5,
-    # deprecated
     "mpt-7b-chat": 0.1,
     "oasst-pythia-12b": 0.1,
     "RWKV-4-Raven-14B": 0.1,
@@ -196,7 +195,7 @@ SAMPLING_WEIGHTS = {
     "chatglm-6b": 0.5,
 }
 
-SAMPLING_BOOST_MODELS = ["llama-2-70b-chat"]
+SAMPLING_BOOST_MODELS = ["llama-2-70b-chat", "codellama-34b-instruct"]
 
 model_pairs = []
 model_pairs_weights = []
